ad66cfa846a505dfe5a2f167c7c7c5ce6971373b
g.vallese@teamsystem.com <g.vallese@teamsystem.com>
2026-03-23 12:21:37 +0100

    chore: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000'], // Your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  });
+
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
